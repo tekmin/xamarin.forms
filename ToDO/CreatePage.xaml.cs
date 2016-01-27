f8355d60036daed0983a40e8ad1dd5a3bc88e971
Ewe Tek Min <aries0604_91@hotmail.com>
2016-01-28 00:04:43 +0800

    Add create page

---
+++ ToDO/CreatePage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ToDO
+{
+	public partial class CreatePage : ContentPage
+	{
+		public CreatePage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
